9b868eb4fb8846333cc129eaf624a3dec956c60d
Peter Lehto <peter@vaadin.com>
2014-04-08 15:59:31 +0300

    Changed the beans.xml to proper folder.

--- ui/src/main/java/org/vaadin/example/VaadinUI.java
+++ ui/src/main/java/org/vaadin/example/VaadinUI.java
@@ -1,31 +1,14 @@
 package org.vaadin.example;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 
 @CDIUI
 @Theme("dawn")
 public class VaadinUI extends UI {
 	private static final long serialVersionUID = 3618386613849364696L;
-
-	@WebServlet(asyncSupported = true, value = "/*")
-	@VaadinServletConfiguration(ui = VaadinUI.class, productionMode = false)
-	public class VaadinApplicationServlet extends VaadinServlet {
-		private static final long serialVersionUID = 4882186678319076880L;
-
-		@Override
-		public void init(ServletConfig servletConfig) throws ServletException {
-			super.init(servletConfig);
-		}
-	}
 
 	@Override
 	protected void init(VaadinRequest request) {
